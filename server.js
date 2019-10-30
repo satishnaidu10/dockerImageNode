fc757135c872bf364df2af168f7866b301af9f25
satishnaidu10 <48701677+satishnaidu10@users.noreply.github.com>
2019-10-30 19:33:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('heloo world');
+  res.send('heloo world this is ramjay');
 });
 
 app.listen(PORT, HOST);
