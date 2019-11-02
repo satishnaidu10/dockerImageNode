9fc22196498e274d1eeb5614bf2e8ddbc8863727
satishnaidu10 <48701677+satishnaidu10@users.noreply.github.com>
2019-11-02 23:55:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+  res.send('This is Heloo World');
 });
 
 app.listen(PORT, HOST);
